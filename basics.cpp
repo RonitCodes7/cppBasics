f5a87401708ff4cf0375240743fc14d37be58db9
RonitCodes7 <rroy8518@gmail.com>
2024-10-06 10:32:50 +0530

    first commit to patterns

--- basics.cpp
+++ basics.cpp
@@ -1,5 +1,13 @@
     #include "./stdc++.h"
     using namespace std;
+
+int  passByValue(int num1, int num2){
+    return num1 + num2;
+}
+int passByReference( int *num1, int *num2){
+    *num1 += 1;
+    return *num1 + *num2;
+}
 int main()
 {
     // int age;
@@ -27,26 +35,72 @@ int main()
     //     cout << "Fail";
     // }
     
-    int a,b,c;
+    // int a,b,c;
 
-    cin >> a >> b >> c;
+    // cin >> a >> b >> c;
 
-    if( a >= b ){
-        if( a > c){
-            cout << "a: "<< a << " is larger" ;
-        }else if( a == c){
-            cout << "both a , c" ;
-        }else{
-            cout << "c: " << c << " is larger";
-        }
-    }else{
-        if( a == b ){
-            cout << "both a , b";
-        }else if( b == c){
-            cout << "both b, c";
-        }else{
-            cout << "b: "<< b << " is larger";
-        }
+    // if( a >= b ){
+    //     if( a > c){
+    //         cout << "a: "<< a << " is larger" ;
+    //     }else if( a == c){
+    //         cout << "both a , c" ;
+    //     }else{
+    //         cout << "c: " << c << " is larger";
+    //     }
+    // }else{
+    //     if( a == b ){
+    //         cout << "both a , b";
+    //     }else if( b == c){
+    //         cout << "both b, c";
+    //     }else{
+    //         cout << "b: "<< b << " is larger";
+    //     }
+    // }
+
+
+
+    // Swtich-Case
+
+    // int num ;
+    // cin >> num;
+    // switch(num){
+    //     case 0:
+    //         cout << "Sunday";
+    //         break;
+    //     case 1:
+    //         cout << "Monday";
+    //         break;
+    //     case 2:
+    //         cout << "Tuesday";
+    //         break;
+    //     case 3:
+    //         cout << "Wednesday";
+    //         break;
+    //     case 4:
+    //         cout << "Thursday";
+    //         break;
+    //     case 5:
+    //         cout << "Friday";
+    //         break;
+    //     case 6:
+    //         cout << "Saturday";
+    //         break;
+    //     default:
+    //         cout << "No day"; 
+    //         break;
+    // }
+
+
+    /* Array 
+    int arr[4] = { 1,2,3,4};
+    for(int i = 0; i < 4; i += 2){
+        cout << arr[i] + arr[i+1]<< endl ;
     }
+    Array ends*/
+
+    cout << passByValue(4,9) << endl;
+    int a = 4, b = 9;
+    cout << passByReference(&a, &b) << endl;
+    cout << a << endl;
     return 0;
 }
